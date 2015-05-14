24c9aa229daa4c5f9cc6f42277917563f1ff060e
Phillip Creglow <phillipcreglow@msn.com>
2015-05-13 22:36:31 -0400

    First attempt at code for display list in app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,37 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-	// Can't wait! - tracer bullet
+	document.head.parentElement.className = "js";
+
+	var elements = document.querySelectorAll('ul');
+	var nestedElements = document.querySelectorAll('li');
+
+
+	_.forEach(elements, function(element, index, elements){
+	  element.addEventListener('view', function(){
+	    //console.log(elements, index, element)
+	    elements[index].parentElement.classList.toggle('todo-list');
+	  });
+	});
+
+	_.forEach(nestedElements, function(element, index, elements){
+	    element.addEventListener('view', function(){
+	        element.parentElement.classList.toggle('todo-list');
+	    });
+	});
 
 })(window);
+
+/*elements[0].addEventListener('click', function(){
+	elements[0].parentElement.classList.toggle('cbp-ntopen');
+});*/
+
+/*for ( var index = 0; index < elements.length; index++ ){
+	var element = elements[index]
+	element.addEventListener('click', function(){
+		console.log(elements, index, elements[index])
+		elements[index].parentElement.classList.toggle('cbp-ntopen');
+	});
+
+}
+console.log(index);*/
